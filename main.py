b08f9d4b7dfb16d72234b436b1ba122f61ce3f25
myrkytyn <maksmykytyn@gmail.com>
2022-09-03 01:05:55 +0300

    added logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 import json
 from tokenize import String
 from xml.etree.ElementTree import tostring
@@ -6,47 +7,69 @@ import os
 import re
 from loguru import logger
 
-excel_source_path="/home/maksym/Maks/Projects/23-VAT-REPORT/excel_source_path/"
-excel_target_path="/home/maksym/Maks/Projects/23-VAT-REPORT/excel_target_path/"
-xml_source_path="/home/maksym/Maks/Projects/23-VAT-REPORT/xml_source_path/"
-xml_target_path="/home/maksym/Maks/Projects/23-VAT-REPORT/xml_target_path/"
-config=json.load(open("config.json","r"))
+excel_source_path = "./excel_source_path/"
+excel_target_path = "./excel_target_path/"
+xml_target_path = "./xml_target_path/"
 
-#Create functionality to add filepath manually
-    #filePath = input('Please enter the path of the folder where the excel files are stored: ')
-#Add check for overwriting
-#Use col_range in remove rows func >>> col_range = ws['C:D'] (refactoring)
-#Think what to co with document number
-#Write instruction for creating M5 report from IIKO
+restaurant_cell = "A6"
+cooking_place = "B"
+dishes_group_col = "C"
+dishes_col = "D"
+quantity_col = "E"
+sum_col = "F"
+sum_without_excise_col = "I"
+sum_without_vat_col = "J"
+price_col = "K"
 
 
 def main():
     logger.info("### PROGRAM STARTED ###")
-    #List all files to var
-    excel_source_file=list_excels(excel_path=excel_source_path)
-    #For all files in dir
+    # Work with config.json
+    #hnum = input("Введіть номер з якого почати нумерувати податкові накладні: ")
+    try:
+        global config
+        config = json.load(open("config.json", "r"))
+    except:
+        logger.error("Config File does not exist. Please check!")
+        return
+    # Get all files from directory
+    excel_source_file = list_excels(excel_source_path)
     for i in range(0, len(excel_source_file)):
-        wb = openpyxl.load_workbook(excel_source_file[i])
-        ws = wb.active
-        unmerge_cells(ws)
-        remove_rows_total(ws)
-        add_formulas(ws)
-        legal_name, non_excise_dishes, non_excise_groups = get_names(ws)
-        non_excise_dishes_formulas(ws, non_excise_dishes)
-        non_excise_groups_formulas(ws, non_excise_groups)
-        date=get_date(ws)
-        wb.save(excel_target_path+date+"_"+excel_source_file[i])
-    logger.info("### EXCEL PART COMPLETED ###")   
+        if excel_source_file[i].endswith(".xlsx"):
+            try:
+                wb = openpyxl.load_workbook(excel_source_file[i])
+            except:
+                logger.error(f"Error opening {excel_source_file[i]} file")
+                return
+            ws = wb.active
+            unmerge_cells(ws)
+            remove_rows_total(ws)
+            remove_rows_zero_price(ws)
+            add_formulas(ws)
+            iiko_name, non_excise_dishes, non_excise_groups = get_info_xlsx(ws)
+            non_excise_dishes_formulas(ws, non_excise_dishes)
+            non_excise_groups_formulas(ws, non_excise_groups)
+            date = get_date(ws)
+            os.chdir("..")
+            if not os.path.exists(excel_target_path):
+                os.makedirs(excel_target_path)
+            try:
+                wb.save(f"{excel_target_path}{date}_{iiko_name}.xlsx")
+            except:
+                logger.error(f"Error saving {excel_source_file[i]} file")
+                return
+    logger.info("### EXCEL PART COMPLETED ###")
 
-    #For all target excels. Working with XML
-    #excel_target_file=list_excels(excel_path=excel_target_path)
-    #for i in range(0, len(excel_target_file)):
+    # For all target excels. Working with XML
+    # excel_target_file=list_excels(excel_path=excel_target_path)
+    # for i in range(0, len(excel_target_file)):
     #    wb = openpyxl.load_workbook(excel_target_file[i])
     #    ws = wb.active
 
-    #logger.info("### XML PART COMPLETED ###") 
+    # logger.info("### XML PART COMPLETED ###")
 
-    logger.info("### PROGRAM COMPLETED ###")                
+    logger.info("### PROGRAM COMPLETED ###")
+
 
 def list_excels(excel_path):
     os.chdir(excel_path)
@@ -54,79 +77,91 @@ def list_excels(excel_path):
     logger.info(f"In directory {excel_path} are files: \n{excel_files}")
     return excel_files
 
+
 def unmerge_cells(ws):
     for merge in list(ws.merged_cells):
         ws.unmerge_cells(range_string=str(merge))
 
+
 def remove_rows_total(ws):
-    #Removing rows with Total in "B" column - Місце приготування 
-    for cell in ws['B']:
+    # Removing rows with Total in column - Місце приготування
+    for cell in ws[cooking_place]:
         if cell.value is not None:
             if "Total" in cell.value:
                 ws.delete_rows(cell.row)
-    #Removing rows with Total in "C" column - Група страви 
-    for cell in ws['C']:
+    # Removing rows with Total in column - Група страви
+    for cell in ws[dishes_group_col]:
         if cell.value is not None:
             if "Total" in cell.value:
                 ws.delete_rows(cell.row)
+
+
+def remove_rows_zero_price(ws):
+    for cell in ws[sum_col]:
+        if cell.value == 0:
+            ws.delete_rows(cell.row)
+
 
 def add_formulas(ws):
-    ws['I5'] = "Сума без акцизу"
-    ws['J5'] = "Сума без ПДВ"
-    ws['K5'] = "Ціна без ПДВ"
-    
-    #Start from D6
-    for row in ws.iter_rows(min_col=4,max_col=4, min_row=6, max_row=ws.max_row):
-        for cell in row:
-            #Find price without excise
-            ws[f'I{cell.row}'] = f"=F{cell.row}/105*100"
-            #Find price without VAT
-            ws[f'K{cell.row}'] = f"=I{cell.row}/6*5"
-            #Find price for 1 product
-            ws[f'J{cell.row}'] = f"=K{cell.row}/E{cell.row}"
+    ws[f"{sum_without_excise_col}5"] = "Сума без акцизу"
+    ws[f"{sum_without_vat_col}5"] = "Сума без ПДВ"
+    ws[f"{price_col}5"] = "Ціна без ПДВ"
 
-#Non excise dishes check
+    # Start from 6
+    for row in ws.iter_rows(min_col=4, max_col=4, min_row=6, max_row=ws.max_row):
+        for cell in row:
+            # Find price without excise
+            ws[f"{sum_without_excise_col}{cell.row}"] = f"={sum_col}{cell.row}/105*100"
+            # Find price without VAT
+            ws[f"{sum_without_vat_col}{cell.row}"] = f"={sum_without_excise_col}{cell.row}/6*5"
+            # Find price for 1 product
+            ws[f"{price_col}{cell.row}"] = f"={sum_without_vat_col}{cell.row}/{quantity_col}{cell.row}"
+
+
 def non_excise_dishes_formulas(ws, non_excise_dishes):
-    counter=0
+    counter = 0
     for cell in ws['D']:
         if cell.value in non_excise_dishes:
             ws[f'I{cell.row}'] = f"=F{cell.row}"
-            counter+=1
-    logger.info(f"excise deleted for {counter} dishes")
+            counter += 1
+    logger.info(f"Excise deleted for {counter} dishes")
 
-#Non excise groups check
+
 def non_excise_groups_formulas(ws, non_excise_groups):
-    counter_groups=0
-    counter_dishes=0
+    counter_groups = 0
+    counter_dishes = 0
     for cell in ws['C']:
         if cell.value in non_excise_groups:
             ws[f'I{cell.row}'] = f"=F{cell.row}"
-            counter_dishes+=1
-            i=1
-            while ws[f"C{cell.row+i}"].value==None:
-                ws[f'I{cell.row}'] = f"=F{cell.row}"
-                counter_dishes+=1
-                i+=1
-            counter_groups+=1
-    logger.info(f"Excise deleted for {counter_dishes} dishes in {counter_groups} groups")
+            counter_dishes += 1
+            i = 1
+            while ws[f"C{cell.row+i}"].value == None:
+                ws[f'I{cell.row+i}'] = f"=F{cell.row+i}"
+                counter_dishes += 1
+                i += 1
+            counter_groups += 1
+    logger.info(
+        f"Excise deleted for {counter_dishes} dishes in {counter_groups} groups")
 
 
-def get_names(ws):
-    restaurant = ws['A6'].value
+def get_info_xlsx(ws):
+    restaurant = ws[restaurant_cell].value
     if restaurant in config["legal_entities"]:
-        iiko_name=config["legal_entities"][restaurant]["iiko_name"]
-        legal_name=config["legal_entities"][restaurant]["legal_name"]
-        non_excise_dishes=config["legal_entities"][restaurant]["non_excise_dishes"]
-        non_excise_groups=config["legal_entities"][restaurant]["non_excise_groups"]
-        logger.info(f"In restaurant {iiko_name} are: \nnon excise dishes - {non_excise_dishes} \nnon excise groups - {non_excise_groups}")
-        return(legal_name, non_excise_dishes, non_excise_groups)
+        iiko_name = config["legal_entities"][restaurant]["iiko_name"]
+        non_excise_dishes = config["legal_entities"][restaurant]["non_excise_dishes"]
+        non_excise_groups = config["legal_entities"][restaurant]["non_excise_groups"]
+        logger.info(
+            f"In restaurant {iiko_name} are: \nnon excise dishes - {non_excise_dishes} \nnon excise groups - {non_excise_groups}")
+        return (iiko_name, non_excise_dishes, non_excise_groups)
     else:
         logger.error(f"{ws['A6'].value} does not exist in JSON. Please check!")
 
+
 def get_date(ws):
-    #date to format ddmmyy
+    # date to format ddmmyyyy
     date = re.sub("[^0-9]", "", ws['A3'].value)
     return date
+
 
 if __name__ == "__main__":
     main()
--- xml_generation.py
+++ xml_generation.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-def generate_xml(tin,c_doc,c_doc_sub,c_doc_ver,c_doc_type,c_doc_cnt,c_reg,c_raj,period_month,period_type,period_year,c_sti_orig,c_doc_stan,d_fill):
+def generate_xml(tin, c_doc_cnt, period_month, period_year, d_fill, hfill, hnamesel, hksel):
     root = ET.Element("DECLAR")
     root.set("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance")
     root.set("xsi:noNamespaceSchemaLocation","J1201013.XSD")
@@ -8,26 +8,47 @@ def generate_xml(tin,c_doc,c_doc_sub,c_doc_ver,c_doc_type,c_doc_cnt,c_reg,c_raj,
     root.append(DECLARHEAD)
     ET.SubElement(DECLARHEAD, "TIN").text=tin
     ET.SubElement(DECLARHEAD, "C_DOC").text="J12"
-    ET.SubElement(DECLARHEAD, "C_DOC_SUB").text=
-    ET.SubElement(DECLARHEAD, "C_DOC_VER").text=c_doc_ver
-    ET.SubElement(DECLARHEAD, "C_DOC_TYPE").text=c_doc_type
+    ET.SubElement(DECLARHEAD, "C_DOC_SUB").text="010"
+    ET.SubElement(DECLARHEAD, "C_DOC_VER").text="13"
+    ET.SubElement(DECLARHEAD, "C_DOC_TYPE").text="0"
+    #100 - Номер документу в періоді, порядковий номер кожного однотипного документу в цьому періоді
     ET.SubElement(DECLARHEAD, "C_DOC_CNT").text=c_doc_cnt
-    ET.SubElement(DECLARHEAD, "C_REG").text=c_reg
-    ET.SubElement(DECLARHEAD, "C_RAJ").text=c_raj
+    ET.SubElement(DECLARHEAD, "C_REG").text="9"
+    ET.SubElement(DECLARHEAD, "C_RAJ").text="15"
     ET.SubElement(DECLARHEAD, "PERIOD_MONTH").text=period_month
-    ET.SubElement(DECLARHEAD, "PERIOD_TYPE").text=period_type
+    ET.SubElement(DECLARHEAD, "PERIOD_TYPE").text="1"
     ET.SubElement(DECLARHEAD, "PERIOD_YEAR").text=period_year
-    ET.SubElement(DECLARHEAD, "C_STI_ORIG").text=c_sti_orig
-    ET.SubElement(DECLARHEAD, "C_DOC_STAN").text=c_doc_stan
+    ET.SubElement(DECLARHEAD, "C_STI_ORIG").text="915"
+    ET.SubElement(DECLARHEAD, "C_DOC_STAN").text="1"
     ET.SubElement(DECLARHEAD, "LINKED_DOCS").set("xsi:nil","true")
     ET.SubElement(DECLARHEAD, "D_FILL").text=d_fill
     ET.SubElement(DECLARHEAD, "SOFTWARE").text="MEDOC"
 
     DECLARBODY=ET.Element("DECLARBODY")
     root.append(DECLARBODY)
+    ET.SubElement(DECLARBODY, "R01G1").set("xsi:nil","true")
+    ET.SubElement(DECLARBODY, "R03G10S").set("xsi:nil","true")
+    ET.SubElement(DECLARBODY, "HORIG1").text="1"
+    ET.SubElement(DECLARBODY, "HTYPR").text="11"
+    ET.SubElement(DECLARBODY, "HFILL").text=hfill
+    ET.SubElement(DECLARBODY, "HNUM").text="1"
+    ET.SubElement(DECLARBODY, "HNUM1").set("xsi:nil","true")
+    ET.SubElement(DECLARBODY, "HNAMESEL").text=hnamesel
+    ET.SubElement(DECLARBODY, "HNAMEBUY").text="Неплатник"
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HNUM2").set("xsi:nil","true")
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
+    ET.SubElement(DECLARBODY, "HKSEL").text=hksel
 
     tree = ET.ElementTree(root)
     tree.write("file.xml", encoding="windows-1251", xml_declaration=True)
 
 if __name__ == "__main__":
-    generate_xml("1","1","1","1","1","1","1","1","1","1","1","1","1","1")
+    generate_xml("1","1","1","1","1")
