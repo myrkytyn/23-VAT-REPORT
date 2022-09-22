cba26b52d3101d032e22c6de021353b831fa2ce6
myrkytyn <maksmykytyn@gmail.com>
2022-09-23 00:26:30 +0300

    logic changed, code refactoring, sum added

--- excel.py
+++ excel.py
@@ -40,36 +40,47 @@ def add_formulas(ws, sum_without_excise_col, sum_without_vat_col, price_col, sum
     ws[f"{price_col}5"] = "Ціна без ПДВ"
     ws[f"{uktzed_col}5"] = "Код УКТЗЕД"
 
+    total_without_excise = 0
+    total_without_vat = 0
     # Start from 6
     for row in ws.iter_rows(min_col=4, max_col=4, min_row=6, max_row=ws.max_row):
         for cell in row:
             # Find price without excise
-            ws[f"{sum_without_excise_col}{cell.row}"] = str(round((float(ws[f"{sum_col}{cell.row}"].value)/105*100),2))
+            sum_without_excise_fl = round(
+                float(ws[f"{sum_col}{cell.row}"].value)/105*100, 2)
+            ws[f"{sum_without_excise_col}{cell.row}"] = str(
+                sum_without_excise_fl)
+            total_without_excise += sum_without_excise_fl
             # Find price without VAT
-            ws[f"{sum_without_vat_col}{cell.row}"] = str(round((float(ws[f"{sum_without_excise_col}{cell.row}"].value)/6*5),2))
+            sum_without_vat_fl = round((float(ws[f"{sum_without_excise_col}{cell.row}"].value)/6*5), 2)
+            ws[f"{sum_without_vat_col}{cell.row}"] = str(sum_without_vat_fl)
+            total_without_vat+=sum_without_vat_fl
             # Find price for 1 product
-            ws[f"{price_col}{cell.row}"] = str(round((float(ws[f"{sum_without_vat_col}{cell.row}"].value)/float(ws[f"{quantity_col}{cell.row}"].value)),2))
+            ws[f"{price_col}{cell.row}"] = str(round((float(
+                ws[f"{sum_without_vat_col}{cell.row}"].value)/float(ws[f"{quantity_col}{cell.row}"].value)), 2))
+    ws[f"{sum_without_excise_col}{ws.max_row+1}"] = total_without_excise
+    ws[f"{sum_without_vat_col}{ws.max_row}"] = total_without_vat
 
 
-def non_excise_dishes_formulas(ws, non_excise_dishes):
+def non_excise_dishes_formulas(ws, non_excise_dishes, sum_col, sum_without_excise_col):
     counter = 0
     for cell in ws['D']:
         if cell.value in non_excise_dishes:
-            ws[f'I{cell.row}'] = f"=F{cell.row}"
+            ws[f"{sum_without_excise_col}{cell.row}"] = ws[f"{sum_col}{cell.row}"].value
             counter += 1
     logger.info(f"Акциз видалено з {counter} страв")
 
 
-def non_excise_groups_formulas(ws, non_excise_groups):
+def non_excise_groups_formulas(ws, non_excise_groups, sum_col, sum_without_excise_col):
     counter_groups = 0
     counter_dishes = 0
     for cell in ws['C']:
         if cell.value in non_excise_groups:
-            ws[f'I{cell.row}'] = f"=F{cell.row}"
+            ws[f'{sum_without_excise_col}{cell.row}'] = ws[f"{sum_col}{cell.row}"].value
             counter_dishes += 1
             i = 1
             while ws[f"C{cell.row+i}"].value == None:
-                ws[f'I{cell.row+i}'] = f"=F{cell.row+i}"
+                ws[f'{sum_without_excise_col}{cell.row+i}'] = ws[f"{sum_col}{cell.row}"].value
                 counter_dishes += 1
                 i += 1
             counter_groups += 1
@@ -94,6 +105,18 @@ def get_dir_name(ws, restaurant_cell, cooking_place_cell):
     dir_name = f"{ws[restaurant_cell].value}-{dir_suffix}"
     return dir_name
 
+
 def put_hnum(ws, text_cell, num_cell, hnum):
     ws[text_cell] = "Номер документу"
     ws[num_cell] = hnum
+
+
+def change_column_width(ws, sum_without_excise_col, sum_without_vat_col, price_col, uktzed_col):
+    ws.column_dimensions[sum_without_excise_col].width = 10
+    ws.column_dimensions[sum_without_vat_col].width = 10
+    ws.column_dimensions[price_col].width = 10
+    ws.column_dimensions[uktzed_col].width = 20
+    ws.column_dimensions["F"].width = 8
+    ws.column_dimensions["G"].width = 8
+    ws.column_dimensions["H"].width = 8
+    ws.column_dimensions["I"].width = 8
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 from loguru import logger
 from xml_generation import generate_xml, generate_root, generate_head, generate_body, generate_b_part, generate_ending
 from json_info import get_info_xlsx, get_info_xml
-from excel import put_hnum, non_excise_groups_formulas, non_excise_dishes_formulas, list_excels, unmerge_cells, remove_rows_total, remove_rows_zero_price, add_formulas, get_date, get_dir_name
+from excel import put_hnum, non_excise_groups_formulas, change_column_width, non_excise_dishes_formulas, list_excels, unmerge_cells, remove_rows_total, remove_rows_zero_price, add_formulas, get_date, get_dir_name
 
 excel_source_path = "./iiko_reports/"
 excel_target_path = "./excel_files_generated/"
@@ -35,7 +35,7 @@ def main():
     logger.info("### ПРОГРАМА РОЗПОЧАЛА РОБОТУ ###")
     excel_part()
     #input("Press Enter to continue...")
-    # xml_part()
+    #xml_part()
     logger.info("### ПРОГРАМА ЗАКІНЧИЛА РОБОТУ ###")
     input("Мені видається, що все пройшло добре. Натисни Enter для виходу")
 
@@ -81,13 +81,14 @@ def excel_part():
                          sum_without_vat_col, price_col, sum_col, quantity_col, uktzed_col)
             iiko_name, non_excise_dishes, non_excise_groups = get_info_xlsx(
                 ws, restaurant_cell, config)
-            non_excise_dishes_formulas(ws, non_excise_dishes)
-            non_excise_groups_formulas(ws, non_excise_groups)
+            non_excise_dishes_formulas(ws, non_excise_dishes, sum_col, sum_without_excise_col)
+            non_excise_groups_formulas(ws, non_excise_groups, sum_col, sum_without_excise_col)
             put_hnum(ws, document_text_cell, document_number_cell, hnum)
             date = get_date(ws, date_cell)[0]
             rest_dir_name = get_dir_name(
                 ws, restaurant_cell, cooking_place_cell)
             file_name = f"{excel_target_path}{rest_dir_name}/{date}_{iiko_name}.xlsx"
+            change_column_width(ws, sum_without_excise_col,sum_without_vat_col,price_col,uktzed_col)
             if not os.path.exists(f"../{excel_target_path}{rest_dir_name}"):
                 os.makedirs(f"../{excel_target_path}{rest_dir_name}")
             try:
